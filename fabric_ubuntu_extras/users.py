6080945dea8a2652dc57d53396452503110d6fcb
Micheil Smith <micheil@brandedcode.com>
2011-02-22 05:43:11 -0800

    Fixed users.add command syntax

--- fabric_ubuntu_extras/users.py
+++ fabric_ubuntu_extras/users.py
@@ -2,9 +2,11 @@ from utils import sudo
 
 
 def add(username, uid=False, verbose=False):
-	cmd = 'useradd --no-create-home --system %s' % username
+	cmd = 'useradd --no-create-home --system'
 	if uid:
 		cmd += '--uid %s' % uid
+	
+	cmd += username
 	
 	return sudo(cmd).succeeded
 
